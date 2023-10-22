a8fce5f3a85fe6193900b3c4366879ec839a14c3
mrvntech <mrvntech@gmail.com>
2023-10-22 15:37:09 +0700

    fix merge config

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import { AxiosResponse } from 'axios'
 import { AuthenticateResponseBody, authenticate } from '@/api/auth'
-=======
-import NextAuth, { Account, Session, User } from 'next-auth'
-import AppleProvider from 'next-auth/providers/apple'
-import FacebookProvider from 'next-auth/providers/facebook'
-import GoogleProvider from 'next-auth/providers/google'
-import EmailProvider from 'next-auth/providers/email'
-import { JWT, getToken } from 'next-auth/jwt'
-import { AdapterUser } from 'next-auth/adapters'
-import CredentialsProvider from "next-auth/providers/credentials"
->>>>>>> 4376d050f7aba4400ce5284841ee661b047e5f33
 
 
 const GOOGLE_AUTHORIZATION_URL =
@@ -25,7 +14,6 @@ const GOOGLE_AUTHORIZATION_URL =
 
 export default NextAuth({
   providers: [
-<<<<<<< HEAD
     // OAuth authentication providers...
     GoogleProvider({
       clientId: process.env.GOOGLE_ID as string,
@@ -46,74 +34,3 @@ export default NextAuth({
     }
   },
 })
-=======
-    CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
-      name: "Credentials",
-      // `credentials` is used to generate a form on the sign in page.
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" }
-      },
-      async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
-        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
-  
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null
-  
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-        }
-      }
-    }),
-    AppleProvider({
-      clientId: process.env.APPLE_ID as string,
-      clientSecret: process.env.APPLE_SECRET as string,
-    }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_ID as string,
-      clientSecret: process.env.FACEBOOK_SECRET as string,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string,
-    }),
-    // Passwordless / email sign in
-  ],
-  callbacks: {
-    async session({ session, token }) {
-      console.log('session: ', session.user)
-      console.log('token', token)
-      if(session.user) {
-        session.user.id = token.id;
-        session.accessToken = token.accessToken;
-      
-      }
-      return session;
-    },
-    async jwt({ token, user, account, profile, isNewUser }) {
-      if (user) {
-        console.log('user: ', user)
-        token.id = user.id;
-      }
-      if (account) {
-        console.log('account: ', account)
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
- 
-  },
-
-}
-  )
-
-
->>>>>>> 4376d050f7aba4400ce5284841ee661b047e5f33
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,42 +4,22 @@ import { Stack } from "@mui/material";
 import { useSession } from "next-auth/react";
 import { Inter } from "next/font/google";
 import { LayOut as Section } from "../layout";
-<<<<<<< HEAD
-import NumberInputIntroduction from "@/components/input-price";
-import { blue } from "@mui/material/colors";
-import TextInput from "@/components/text-input";
-import FilterBar from "@/features/all-services/filter-bar";
-import Title from "@/components/title";
-import axios from "axios";
-=======
 import NewsPage from "@/features/home/NewsPage";
 import LayOutWithOnlyHeader from "@/layout/layoutWithOnlyHeader";
->>>>>>> 4376d050f7aba4400ce5284841ee661b047e5f33
 
 const inter = Inter({ subsets: ["latin"] });
 
-async function callBackendDemo() {
-  const result = await axios.post("http://localhost:1337/api/users-permissions/auth/login", {
-    username: "phucnq@ttlab.com",
-    password: "Ctsv@2021"
-  })
-  console.log(result);
-}
+// async function callBackendDemo() {
+//   const result = await axios.post("http://localhost:1337/api/users-permissions/auth/login", {
+//     username: "phucnq@ttlab.com",
+//     password: "Ctsv@2021"
+//   })
+//   console.log(result);
+// }
 
 export default function Home() {
-<<<<<<< HEAD
-  return (
-    <Section>
-      <Box>
-        <Title value="Tất cả dịch vụ" />
-        <FilterBar />
-      </Box>
-      <Button onClick={() => {
-        callBackendDemo()
-      }}>click me !!</Button>
-    </Section>
-  );
-=======
+  const { data } = useSession();
+  console.log(data);
   const { data: session, status } = useSession();
   console.log(session, status);
   
@@ -52,6 +32,5 @@ export default function Home() {
         </LayOutWithOnlyHeader>
       </>
     );
->>>>>>> 4376d050f7aba4400ce5284841ee661b047e5f33
 }
 
